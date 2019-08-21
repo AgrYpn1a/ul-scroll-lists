b8f9655f6191cde1160d399944f273d00e0693d9
rtojagic <rastkotojagic1996@gmail.com>
2019-08-21 17:15:02 +0200

    Inital commit;

---
+++ Assets/Scripts/Editor/RadialListEditor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(RadialList))]
+public sealed class RadialListEditor : Editor
+{
+    private RadialList _rl;
+
+    private void OnEnable()
+    {
+        EditorApplication.hierarchyChanged -= HandleHierarchyChange;
+        EditorApplication.hierarchyChanged += HandleHierarchyChange;
+    }
+
+    private void HandleHierarchyChange()
+    {
+
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        _rl = target as RadialList;
+
+        float angleDelta = 360f / (_rl.content.childCount + 1);
+        float angle = 0f + _rl.rotation;
+
+        float maxAngle = -180f;
+
+        if (_rl.direction == RadialList.Direction.LEFT_TO_RIGHT)
+        {
+            maxAngle = 180f;
+            angle += maxAngle;
+        }
+
+        foreach (RectTransform element in _rl.content)
+        {
+            angle += angleDelta;
+
+            Debug.Log("Child " + element);
+            //element.anchoredPosition = new Vector3(_rl.radius * Mathf.Cos(angle * Mathf.Deg2Rad), _rl.radius * Mathf.Sin(angle * Mathf.Deg2Rad), 0f);
+            element.localPosition = new Vector2(
+            _rl.radius * Mathf.Cos(angle * Mathf.Deg2Rad),
+            _rl.radius * Mathf.Sin(angle * Mathf.Deg2Rad));
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/RadialList.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class RadialList : MonoBehaviour
+{
+
+    public enum Direction
+    {
+        LEFT_TO_RIGHT,
+        RIGHT_TO_LEFT
+    }
+
+    private RectTransform _rect;
+    public Transform content;
+
+    [Space(4)]
+    [Header("References")]
+    [SerializeField]
+    public float radius = 1f;
+    public float rotation = 100f;
+    public Direction direction = Direction.LEFT_TO_RIGHT;
+
+    private void Awake()
+    {
+        _rect = GetComponent<RectTransform>();
+    }
+
+    private void Start()
+    {
+
+    }
+
+}
