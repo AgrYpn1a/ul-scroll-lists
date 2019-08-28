b52b8b8311c18b8149c8385a031a45ae920123d7
rtojagic <rastkotojagic1996@gmail.com>
2019-08-28 21:46:58 +0200

    Working example

--- Assets/Scripts/Editor/RadialListEditor.cs
+++ Assets/Scripts/Editor/RadialListEditor.cs
@@ -6,47 +6,28 @@ using UnityEngine;
 [CustomEditor(typeof(RadialList))]
 public sealed class RadialListEditor : Editor
 {
-    private RadialList _rl;
+    private static readonly HashSet<RadialList> _radialLists = new HashSet<RadialList>();
 
-    private void OnEnable()
+    static RadialListEditor()
     {
-        EditorApplication.hierarchyChanged -= HandleHierarchyChange;
-        EditorApplication.hierarchyChanged += HandleHierarchyChange;
+        EditorApplication.hierarchyChanged += UpdateView;
     }
 
-    private void HandleHierarchyChange()
+    private void Awake()
     {
+        _radialLists.Add(target as RadialList);
+    }
 
+    private static void UpdateView()
+    {
+        foreach (var rl in _radialLists)
+            rl.UpdateListView();
     }
 
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
 
-        _rl = target as RadialList;
-
-        float angleDelta = 360f / (_rl.content.childCount + 1);
-        float angle = 0f + _rl.rotation;
-
-        float maxAngle = -180f;
-
-        if (_rl.direction == RadialList.Direction.LEFT_TO_RIGHT)
-        {
-            maxAngle = 180f;
-            angle += maxAngle;
-        }
-
-        foreach (RectTransform element in _rl.content)
-        {
-            angle += angleDelta;
-
-            Debug.Log("Child " + element);
-            //element.anchoredPosition = new Vector3(_rl.radius * Mathf.Cos(angle * Mathf.Deg2Rad), _rl.radius * Mathf.Sin(angle * Mathf.Deg2Rad), 0f);
-            element.localPosition = new Vector2(
-            _rl.radius * Mathf.Cos(angle * Mathf.Deg2Rad),
-            _rl.radius * Mathf.Sin(angle * Mathf.Deg2Rad));
-
-        }
+        UpdateView();
     }
-
 }
